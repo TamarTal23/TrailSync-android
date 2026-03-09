74d994cb3fa2cbe64bb407c3a22af11551738b23
TamarTal23 <212927487@idf.il>
2026-03-09 08:44:10 +0200

    add navigation from profile page to edit profile fragment

--- app/src/main/java/com/idz/trailsync/ProfileFragment.kt
+++ app/src/main/java/com/idz/trailsync/ProfileFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val editProfileButton: Button = view.findViewById(R.id.editProfileButton)
+        editProfileButton.setOnClickListener {
+            findNavController().navigate(R.id.editProfileFragment)
+        }
+        return view
     }
 }
